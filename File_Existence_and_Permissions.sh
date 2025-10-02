9310197445f3c5deb750cb1d74ccac3c5b3370dd
unknown <eshwarkodavali123@gmail.com>
2025-10-02 15:45:29 +0530

    1st assignment

--- File_Existence_and_Permissions.sh
+++ File_Existence_and_Permissions.sh
@@ -11,14 +11,12 @@ if [ -r "$FILE_NAME" ]; then
     echo "$FILE_NAME is readable."
 else
     echo "$FILE_NAME is not readable."
-    exit 1
 fi
 
 if [ -w "$FILE_NAME" ]; then
     echo "$FILE_NAME is writable."
 else
     echo "$FILE_NAME is not writable."
-    exit 1
 fi
 
 
