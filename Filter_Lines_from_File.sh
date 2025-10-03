67a62e050f3497dc69dfe99a916c4fff35d3e6ea
unknown <eshwarkodavali123@gmail.com>
2025-10-04 02:51:44 +0530

    line

---
+++ Filter_Lines_from_File.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SOURCE_FILE=/tmp/test/source
+DESTINATION_FILE=/tmp/test/destination
+SEARCH_WORD="ERROR"
+
+while IFS=read -r line;
+do
+    echo "$line" > "$DESTINATION_FILE"
+    exit 1
+done < "$SOURCE_FILE"
+
+while IFS=read -r line;
+do
+    if [[ "$line" == *"$SEARCH_WORD"* ]]; then
+    echo "$line" >> "$DESTINATION_FILE"
+    fi
+done < "$SOURCE_FILE"
